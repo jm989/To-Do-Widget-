2702f074168ccb28b0f11773db9975a23440b6dc
jm989 <145062461+jm989@users.noreply.github.com>
2025-08-12 14:07:39 +0100

    added localStorage implementation to save actions

--- script.js
+++ script.js
@@ -3,27 +3,33 @@ let taskList = document.getElementById("list-of-tasks");
 let inputArea = document.getElementById("task-description");
 let text;
 
-let tasks = [];
+const savedTasksJSON = localStorage.getItem("tasks");
 
-taskButton.addEventListener("click", () => {
-    text = inputArea.value;
+if (savedTasksJSON) {
+    const savedTasks = JSON.parse(savedTasksJSON);
+
+    savedTasks.forEach((taskText) => {
+        let taskCreated = createTask(taskText);
+        taskList.prepend(taskCreated);
+    });
+};
+
+function createTask(text) {
     let task = document.createElement("li");
     let span = document.createElement("span");
     span.textContent = text;
     task.appendChild(span);
-    tasks.push(span.textContent);
     let deleteButton = document.createElement("button");
     deleteButton.textContent = "Del";
     task.appendChild(deleteButton);
     let editButton = document.createElement("button");
     editButton.textContent = "Edit"
     task.appendChild(editButton);
-
-    taskList.prepend(task);
-    inputArea.value = "";
-
+    
+    
     deleteButton.addEventListener("click", () => {
         task.remove();
+        saveTasks();
     });
     
     editButton.addEventListener("click", () => {
@@ -31,19 +37,41 @@ taskButton.addEventListener("click", () => {
             let new_input = document.createElement("input");
             new_input.value = span.textContent;
             task.replaceChild(new_input, span);
-    
             editButton.textContent = "Save";
         }else {
             let currentInput = task.querySelector("input");
             span.textContent = currentInput.value;
             task.replaceChild(span, currentInput);
             editButton.textContent = "Edit"
+            saveTasks();
         }
     })
-
+    
     span.addEventListener("click", () => {
         span.classList.toggle("completed");
+        saveTasks();
     })
+
+    return task
+};
+
+taskButton.addEventListener("click", () => {
+    text = inputArea.value;
+    let newTaskElement = createTask(text);
+    taskList.prepend(newTaskElement);
+    saveTasks();
+    inputArea.value = "";
 });
+
+const saveTasks = () => {
+    let tasks = [];
+    let allTasks = document.querySelectorAll("span");
+    allTasks.forEach((task) => {
+        tasks.push(task.textContent)
+    })
+
+    const task_json = JSON.stringify(tasks);
+    localStorage.setItem("tasks", task_json);
+};
 
 
